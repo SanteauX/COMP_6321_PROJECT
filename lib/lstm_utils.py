d598654cadc3c3b2b48ae066401ebfaa1667daa2
Illya Reutskyy <iliyareutsky@gmail.com>
2021-12-11 21:05:03 -0500

    Some LSTM fixes

--- lib/lstm_utils.py
+++ lib/lstm_utils.py
@@ -1,13 +1,6 @@
 import pandas as pd
 
-def zero_out(timestamp, value):
-    before_april = (timestamp.month == 4 and timestamp.day < 15) or (timestamp.month < 4)
-    after_october = (timestamp.month > 10)
-    if before_april or after_october:
-        value = 0.0
-    return value    
-
-def perform_cv(model, input_series, covariates):
+def cv_predict(model, input_series, covariates):
     return model.historical_forecasts(input_series,
                                       future_covariates=covariates,
                                       forecast_horizon=365,
